794948aad9ac7cbb259ebeff0ab3d763d53d9f57
a-oluk <a_oluk@hotmail.com>
2022-12-07 13:28:58 +0100

    03.12

---
+++ contourf_pcolormesh_test.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate
+import pandas as pd
+
+x = np.array([-10,-5,-1,2,3,10,20,22,25,30,-5,-1,2,3,10,20,22,25,30,18,17,12])
+y = np.array([-3,-3,-2,2,5,6,7,10,14,22,6,7,10,14,22,6,7,10,14,22,6,7])
+
+
+z = (y-2)**2+ (x-4)**3 + np.random.normal(0,1,len(x))
+f = interpolate.interp2d(x, y, z, kind = 'cubic')
+Z2 = f(x, y)
+
+fig = plt.figure(1)
+
+plt.scatter(x,y,s=200,c= z, cmap= "viridis")
+plt.colorbar()
+d = {'X': x, 'Y': y}
+df = pd.DataFrame(data=d)
+final_df = df.sort_values(by=['X'], ascending=True)
+xdata = df['X'].to_numpy()
+ydata= df['Y'].to_numpy()
+
+X2, Y2 = np.meshgrid(xdata, ydata)
+#print(len(x),len(y),len(Z2))
+fig = plt.figure(2)
+plt.pcolormesh(xdata,ydata,Z2)
+plt.colorbar()
+
+#
+plt.figure(3)
+plt.contourf(Z2)
+#plt.axes([0 ,30,0,20])
+plt.show()
+
--- develep.py
+++ develep.py
@@ -2,6 +2,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
+from matplotlib.ticker import LinearLocator
 from IPython.display import display  #
 from scipy.stats import norm
 import statistics
@@ -9,18 +10,32 @@ from scipy.interpolate import *
 
 
 def plot_3_dim():
-
+    # ZUFÄLLIGE TRAININGSDATEN NICHT SO GUT
     xdata = np.random.rand(1000) * 10
     ydata = np.random.rand(1000) * 10
-    zdata = (xdata-5)**2 + (ydata-5)**2 -2 + np.random.normal(0, 1, len(xdata))
+
+    # MIT LINSPACE FUNKTIONIERT SEHR GUT
+    #xdata = np.linspace(1,10,10)
+    #ydata = np.linspace(1, 10, 10)
+
+    zdata = (xdata-5)**2 + (ydata-5)**2+ 0.2* np.random.normal(0, 1, len(xdata))
+
+    d = {'X': xdata, 'Y': ydata}
+    df = pd.DataFrame(data=d)
+    final_df = df.sort_values(by=['X'], ascending=True)
+    xdata = df['X'].to_numpy()
+    ydata= df['Y'].to_numpy()
+
     X,Y = np.meshgrid(xdata, ydata)
-    Z = 2*(X-2)**2 + 3*(Y-2)**2 + np.random.normal(0, 1, len(xdata))
+    Z = 0.2*(X-5)**2 + 0.2*(Y-5)**2 #0.2*np.random.normal(0, 1, len(xdata))
 
     fig = plt.figure(figsize=(9, 6))
     ax = plt.axes(projection='3d')
 
-    #ax.plot_surface(xdata, ydata, zdata, rstride=1, cstride=1,cmap='viridis', edgecolor='none')
-    ax.scatter(xdata, ydata, zdata, c=xdata, cmap='viridis', linewidth=0.5);
+    #surf = ax.plot_surface(X, Y, Z,cmap='viridis')
+    #ax.set_zlim(0, 5)
+    #fig.colorbar(surf, shrink=0.5, aspect=5)
+    ax.scatter(xdata, ydata, zdata, c=zdata, cmap='viridis', linewidth=0.5);
 
     # Give labels
     ax.set_xlabel('x')
